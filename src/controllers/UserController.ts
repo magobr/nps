3b5ab0ae7497afe608974cf5a1ee7daf17563356
Thiago Novaes <novaesthago@gmail.com>
2021-04-19 02:08:37 -0300

    Testes, Refactor controllers

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UserRepository';
 
 class UserController {
     async create(request: Request, response: Response){
         const { name, email } = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const usersAlreadyExists = await usersRepository.findOne({
             email
-        })
+        });
 
         if (usersAlreadyExists) {
             return response.status(400).json({
@@ -20,13 +20,13 @@ class UserController {
 
         const user = usersRepository.create({
             name, 
-            email
-        })
+            email 
+        });
 
         await usersRepository.save(user);
 
-        return response.json(user)
+        return response.status(201).json(user)
     }
 }
 
-export { UserController }
+export { UserController };
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,12 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+export default async (): Promise<Connection> =>{
+    const defaultOpions = await getConnectionOptions();
+    return createConnection(
+        Object.assign(defaultOpions, {
+            database: process.env.NODE_ENV === "test"
+            ? "./src/database/database.test.sqlite" 
+            : defaultOpions.database
+        })
+    )
+}
