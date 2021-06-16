be6b1b2e45d7ccbf9d9315d9192f4e9d8c53e60e
Thiago Novaes <novaesthago@gmail.com>
2021-06-16 11:25:00 -0300

    Envio de e-mail

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailcontroller';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from "./controllers/UserController";
 
@@ -6,10 +7,11 @@ const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveysController();
-
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
